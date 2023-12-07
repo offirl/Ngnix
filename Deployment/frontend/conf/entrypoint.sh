fb01592374ea403d2c45df2adbaf36a8dce67831
Offir Livnat <offirlivnat@gmail.com>
2023-12-07 13:08:07 +0000

    test

--- Deployment/frontend/conf/entrypoint.sh
+++ Deployment/frontend/conf/entrypoint.sh
@@ -3,5 +3,5 @@
 set -eu
 
 echo "update Nginx config"
-envsubst '\${HOST1} \${HOST2} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf
+envsubst '\${HOST1} \${HOST3} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf
 exec "$@"
